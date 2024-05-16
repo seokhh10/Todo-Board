d23f64baf62771fa1f13d41652dd3deee6733327
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-05-15 20:10:23 -0500

    add task button store information to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+  const cardTask = $('<div').addClass('card project-card draggable my-3').attr('data-project-id', task.id);
 
 }
 
@@ -19,27 +20,34 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 $(document).ready(function(){
-    $('#addTask').click(function(){
-    $('#myModal').modal('show');
-    });
+    $('#addTask').click(function(){    //document is link with html, function is telling to the button addTask when click
+    $('#myModal').modal('show');       //open modal to enter a new task. 
+    });    
 
-    $('#myModal').on('click', '#taskSubmit', function(event){
-      event.preventDefault();
-
+    $('#myModal').on('click', '#taskSubmit', function(event){    //modal is telling when you click button (addtask)
+      event.preventDefault();                                    //function(event) is typically passed to event handler when occurs (click,submit, or keypress)  
+                                                                 //will prevent the form from being submitted in the usual way, effectively preventing the browser from navigating to a new page.
     const taskTitle = $('#task-title').val();
     const taskDueDate = $('#task-due-date').val();
-    const taskDescription = $('#task-description').val();
+    const taskDescription = $('#task-description').val();      //# The value is assigned to the variable, val()-method it recover the value of the selected element.
 
       
       const newTask = {
-        id: crypto.randomUUID(),
-        title: taskTitle,
+        id: crypto.randomUUID(),     //creates a new task object with various properties such as ID, 
+        title: taskTitle,            //title,due-date,description, and status, initializing them with values obtained from variables.
         dueDate: taskDueDate,
         description: taskDescription,
         status:'to-do',
     }
 
    console.log(newTask);
+
+   let tasks = JSON.parse(localStorage.getItem('tasks')) || [];  //recover any existing tasks from local storage, converts them from JSON format into a JavaScript array, 
+   tasks.push(newTask);                                          //and then add a new task to this array.
+
+   localStorage.setItem('tasks', JSON.stringify(tasks));
+
+   renderTaskList();     //is a function responsible for generating and displaying a list of task un a user interface (webpage,mobile app)
     })
   });
 // Todo: create a function to handle deleting a task
