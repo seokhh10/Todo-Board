38a96c0dac53651bbb21e7d0a97f063bbbabdb88
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-05-17 20:30:41 -0500

    todo card displayed an empty card, without any data from local storage, working on it

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ function generateTaskId() {
 // Todo: create a function to create a task card
 function createTaskCard(task) {
   const cardTask = $('<div>').addClass('card project-card draggable my-3').attr('data-project-id', task.id);
-  const headerCard = $('<div>').addClass('card-header h4').text(task.title); 
+  const headerCard = $('<div>').addClass('card-header h4').text(task.title);
   const bodyCard = $('<div>').addClass('card-body');
   const descriptionCard = $('<p>').addClass('card-text').text(task.description)
   const dueDateCard = $('<p>').addClass('card-text').text(task.dueDate)
@@ -19,67 +19,87 @@ function createTaskCard(task) {
 
   if (task.dueDate && task.status !== 'done') {
     const now = dayjs();
-    const taskDueDate = dayjs(task.dueDate,  'MM/DD/YYYY');
+    const taskDueDate = dayjs(task.dueDate, 'MM/DD/YYYY');
 
     if (now.isSame(taskDueDate, 'day')) {
-       cardTask.addClass('bg-warning text-white');
+      cardTask.addClass('bg-warning text-white');
     } else if (now.isAfter(taskDueDate)) {
-       cardTask.addClass('bg-danger text-white');
-       deleteCard.addClass('border-light');
+      cardTask.addClass('bg-danger text-white');
+      deleteCard.addClass('border-light');
     }
-    }
-
-    bodyCard.append(descriptionCard, dueDateCard, deleteCard);
-    cardTask.append(headerCard, bodyCard);
-        
-        return cardTask;
   }
+
+  bodyCard.append(descriptionCard, dueDateCard, deleteCard);
+  cardTask.append(headerCard, bodyCard);
+
+  return cardTask;
+}
 
 
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+  const task = JSON.parse(localStorage.getItem('task')) || [];
+
+  const todos = $('#todo-cards');
+  todos.empty();
+
+  const progressList = $('#in-progress-cards');
+  progressList.empty();
+
+  const doneList = $('#done-cards');
+  doneList.empty();
+
+  todos.append(createTaskCard(task));
 
 }
+
+
+  
+
+  
 
 
 
 
 
 // Todo: create a function to handle adding a new task
-$(document).ready(function(){
-    $('#addTask').click(function(){    //document is link with html, function is telling to the button addTask when click
+$(document).ready(function () {
+  $('#addTask').click(function () {    //document is link with html, function is telling to the button addTask when click
     $('#myModal').modal('show');       //open modal to enter a new task. 
-    });    
+  });
 
-    $('#myModal').on('click', '#taskSubmit', function(event){    //modal is telling when you click button (addtask)
-      event.preventDefault();                                    //function(event) is typically passed to event handler when occurs (click,submit, or keypress)  
-                                                                 //will prevent the form from being submitted in the usual way, effectively preventing the browser from navigating to a new page.
+  $('#myModal').on('click', '#taskSubmit', function (event) {    //modal is telling when you click button (addtask)
+    event.preventDefault();                                    //function(event) is typically passed to event handler when occurs (click,submit, or keypress)  
+    //will prevent the form from being submitted in the usual way, effectively preventing the browser from navigating to a new page.
     const taskTitle = $('#task-title').val();
     const taskDueDate = $('#task-due-date').val();
     const taskDescription = $('#task-description').val();      //# The value is assigned to the variable, val()-method it recover the value of the selected element.
 
-      
-      const newTask = {
-        id: crypto.randomUUID(),     //creates a new task object with various properties such as ID, 
-        title: taskTitle,            //title,due-date,description, and status, initializing them with values obtained from variables.
-        dueDate: taskDueDate,
-        description: taskDescription,
-        status:'to-do',
+
+    const newTask = {
+      id: crypto.randomUUID(),     //creates a new task object with various properties such as ID, 
+      title: taskTitle,            //title,due-date,description, and status, initializing them with values obtained from variables.
+      dueDate: taskDueDate,
+      description: taskDescription,
+      status: 'to-do',
     }
 
-   console.log(newTask);
+    console.log(newTask);
 
-   let tasks = JSON.parse(localStorage.getItem('tasks')) || [];  //recover any existing tasks from local storage, converts them from JSON format into a JavaScript array, 
-   tasks.push(newTask);                                          //and then add a new task to this array.
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];  //recover any existing tasks from local storage, converts them from JSON format into a JavaScript array, 
+    tasks.push(newTask);                                          //and then add a new task to this array.
 
-   localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 
-   renderTaskList();     //is a function responsible for generating and displaying a list of task un a user interface (webpage,mobile app)
-    })
-  });
+    renderTaskList(tasks);     //is a function responsible for generating and displaying a list of task un a user interface (webpage,mobile app)
+  })
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];  //recover any existing tasks from local storage, converts them from JSON format into a JavaScript array, 
+
+  renderTaskList(tasks);
+});
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
