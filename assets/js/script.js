be0aab441a33ac46da1fae03ede30c1264356f2a
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-05-19 19:13:03 -0500

    started working on dropping a task into a card, in-progress or done

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,14 +131,17 @@ function handleDeleteTask() {
   renderTaskList();
 }
 
-
+// Todo: create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+   
+}
 
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
    renderTaskList();
 
-   handleAddTask();
+   handleDeleteTask();
 
    $('.lane').droppable({
     accept: '.draggable',
